690363c30e5c6802b2913ddcbf5c45e430d058d9
Airei Fukuzawa <eyeray@fb.com>
2022-06-27 09:34:34 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Airei Fukuzawa on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ParseChat/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by Airei Fukuzawa on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
